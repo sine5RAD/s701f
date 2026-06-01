54a00e7480248bcdf2bc840af41f4bf5cba978a5
sine5RAD <sinefive@outlook.com>
2026-06-01 03:23:05 +0000

    Deploying to pages from @ sine5RAD/s701f@3fa3020fedf0d0f5efe31526f5396229bec96aac 🚀

---
+++ _astro/url-utils.Du-gQnQy.js
@@ -0,0 +1,1 @@
+import"./config.be-l5Oq-.js";import"./zh_TW.BkvVd5vU.js";function c(o,e){const n=o.replace(/^\/|\/$/g,"").toLowerCase(),r=e.replace(/^\/|\/$/g,"").toLowerCase();return n===r}function t(...o){return o.join("/").replace(/\/+/g,"/")}function u(o){return t("","/",o)}export{c as p,u};
